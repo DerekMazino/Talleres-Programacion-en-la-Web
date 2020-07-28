8a64fe7614a31589173a69327c12f6d1e35b9c28
Deyci <deycigisela.9@hotmail.com>
2020-07-28 14:23:23 -0500

    epqll

---
+++ Parcial_1/jsqform.js
@@ -0,0 +1,30 @@
+$(document).ready(function action(){
+    parmURL();
+});
+function parmURL() {
+    /* Obtener la url completa */
+    url = document.URL;
+    /* Buscar a partir del signo de interrogación ? */
+    url = String(url.match(/\?+.+/));
+    /* limpiar la cadena quitándole el signo ? */
+    url = url.replace("?", "");
+    /* Crear un array con parametro=valor */
+    url = url.split("&");   
+    let epll; 
+    epll=new Array(5);
+    x = 0;
+    while (x < url.length){
+        p = url[x].split("=");
+        meses[x]=p[1];
+        x++;
+    }
+    console.log(epll);
+    $("#Alimento").text(epll[0]);
+    $("#Calorias").text(epll[1]);
+    $("#Grasas").text(epll[2]);
+    $("#Proteinas").text(epll[3]);
+    $("#Carbohidratos").text(epll[4]);
+    if(epll[5]=="2"){
+        $("#imagen").attr("src","meh.png");
+    }
+}
--- Parcial_1/jsqidx.js
+++ Parcial_1/jsqidx.js
@@ -1,10 +1,11 @@
 let flag=0;
 $(document).ready(function(){
     $("input[type=text]").hide();
+    $("input[type=text]").prop("disabled", true)
     $("select").hide();
+    $("select").prop("disabled", true)
     $("p").hide();
     $("#opc").hide();
-
     $("#edit_button1").click(fila1);
     $("#edit_button2").click(fila2);
     $("#edit_button3").click(fila3);
@@ -14,23 +15,29 @@ $(document).ready(function(){
 
 function fila1 (){
     if(flag==0){
-    $("label#food_l1").hide();
-    $("input#food_l1").show("swing");
-    $("label#cal_l1").hide();
-    $("input#cal_l1").show("swing");
-    $("label#gre_l1").hide();
-    $("input#gre_l1").show("swing");
-    $("label#pro_l1").hide();
-    $("input#pro_l1").show("swing");
-    $("label#car_l1").hide();
-    $("input#car_l1").show("swing");
-    $("#img1").hide();
-    $("#select1").show("swing");
-    $("#edit_button1").hide();
-    $("#p1").show("swing");
-    $("#opc").show("swing");
-    flag=1;
-    }
+        $("label#food_l1").hide();
+        $("input#food_l1").show("swing");
+        $("input#food_l1").prop("disabled", false);
+        $("label#cal_l1").hide();
+        $("input#cal_l1").show("swing");
+        $("input#cal_l1").prop("disabled", false);
+        $("label#gre_l1").hide();
+        $("input#gre_l1").show("swing");
+        $("input#gre_l1").prop("disabled", false);
+        $("label#pro_l1").hide();
+        $("input#pro_l1").show("swing");
+        $("input#pro_l1").prop("disabled", false);
+        $("label#car_l1").hide();
+        $("input#car_l1").show("swing");
+        $("input#car_l1").prop("disabled", false);
+        $("#img1").hide();
+        $("#select1").prop("disabled", false);
+        $("#select1").show("swing");
+        $("#edit_button1").hide();
+        $("#p1").show("swing");
+        $("#opc").show("swing");
+        flag=1;
+        }
     else{
         alert("Solo puedes editar una fila a la vez");
     }   
@@ -49,6 +56,7 @@ function fila2 (){
     $("label#car_l2").hide();
     $("input#car_l2").show("swing");
     $("#img2").hide();
+    $("#select2").prop("disabled", false);
     $("#select2").show("swing");
     $("#edit_button2").hide();
     $("#p2").show("swing");
@@ -73,6 +81,7 @@ function fila3 (){
     $("label#car_l3").hide();
     $("input#car_l3").show("swing");
     $("#img3").hide();
+    $("#select3").prop("disabled", false);
     $("#select3").show("swing");
     $("#edit_button3").hide();
     $("#p3").show("swing");
@@ -97,6 +106,7 @@ function fila4 (){
     $("label#car_l4").hide();
     $("input#car_l4").show("swing");
     $("#img4").hide();
+    $("#select4").prop("disabled", false);
     $("#select4").show("swing");
     $("#edit_button4").hide();
     $("#p4").show("swing");
@@ -109,8 +119,50 @@ function fila4 (){
 }
 function refClick(){
     $("input[type=text]").hide(500);
+    $("input[type=text]").prop("disabled", true);
     $("select").hide(500);
+    $("select").prop("disabled", true);
     $("p").hide(500);
     $("#opc").hide(500);
+    mostrarCampos();
     flag=0;
 }
+function mostrarCampos(){
+    $("#img1").show();
+    $("#img2").show();
+    $("#img3").show();
+    $("#img4").show();
+
+    $("label#food_l1").show();
+    $("label#cal_l1").show();
+    $("label#gre_l1").show();
+    $("label#pro_l1").show();
+    $("label#car_l1").show();
+    $("#img1").show();
+    $("#edit_button1").show();
+    
+    $("label#food_l2").show();
+    $("label#cal_l2").show();
+    $("label#gre_l2").show();
+    $("label#pro_l2").show();
+    $("label#car_l2").show();
+    $("#img2").show();
+    $("#edit_button2").show();
+
+    $("label#food_l3").show();
+    $("label#cal_l3").show();
+    $("label#gre_l3").show();
+    $("label#pro_l3").show();
+    $("label#car_l3").show();
+    $("#img3").show();
+    $("#edit_button3").show();
+
+    $("label#food_l4").show();
+    $("label#cal_l4").show();
+    $("label#gre_l4").show();
+    $("label#pro_l4").show();
+    $("label#car_l4").show();
+    $("#img4").show();
+    $("#edit_button4").show();
+}
+
